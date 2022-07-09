a760f7fc8198992a3dd1755385434277ec77fdbe
ViYeM <vitoria.miao@sga.pucminas.br>
2022-07-09 17:20:58 -0300

    Create forum.js

---
+++ codigo/js/forum.js
@@ -0,0 +1,133 @@
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Falar em público",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "José Ferreira",
+                date: Date.now(),
+                content: "Oi gente! Eu abri esse fórum porque eu queria compartilhar algo que aconteceu comigo essa semana. Segunda feira eu tinha que apresentar um trabalho na faculdade. Eu preparei toda a apresentação em casa, só que na hora que eu fui apresentar para a turma, eu não consegui. Eu falei baixo, minha voz começou a tremer e eu esqueci completamente o que eu estava falando, e aí o tempo da apresentação acabou e o professor pediu para eu parar de apresentar. Provavelmente não vou tirar uma nota boa nesse trabalho :( "
+            },
+            {
+                author: "Ana Borges",
+                date: Date.now(),
+                content: "Nossa, eu já passei por uma situação parecida, foi no semestre passado. Eu também tive que apresentar um trabalho para a turma, só que na hora eu estava falando muito baixo, e aí um menino gritou para eu falar mais alto porque ninguém tava me escutando. Todo mundo começou a rir e eu não consegui terminar a apresentação. Na semana seguinte eu teria que apresentar mais um trabalho, e aí eu estava fazendo os desafios do Timidex e um desafio era apresentar algo para um amigo ou parente meu. E ai eu treinei minha apresentação dessa forma. Eu percebi que eu me sai melhor nessa apresentação do que na outra. Tenta fazer isso na próxima vez ;) "
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Não consigo ir à festas",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content 2",
+        comments: [
+            {
+                author: "Sarah Mendes",
+                date: Date.now(),
+                content: "Ei galera! Preciso de um conselho de vocês. Eu não sou muito uma pessoa de socializar. Na verdade, eu não saio de casa, não converso com ninguém fora do meu vínculo social e não tenho muitos amigos. Eu vejo meus colegas indo para bares e festas, e eu queria muito ir. Já me chamaram algumas vezes, mas eu tenho vergonha. Não me sinto confortável em lugares com muitas pessoas que eu não conheço e também tenho medo de ser julgada por não saber dançar ou coisa do tipo. O que eu faço???"
+            },
+            {
+                author: "Fernanda Lopes",
+                date: Date.now(),
+                content: "Oi Sarah!!! Eu te entendo completamente. Nos primeiros períodos da faculdade eu via um monte de gente indo para festas, mas eu não tinha coragem de ir pelo mesmo motivo que o seu. Um dia uma amiga minha me convidou para ir à uma festa e eu disse que não queria, e aí ela me disse que eu deveria ir e aproveitar sem me preocupar com o que as pessoas estão pensando sobre mim, até porque se eles eram desconhecidos provavelmente eu os não veria novamente. Depois do que ela falou, eu fui à festa e me diverti muito. Com certeza teria me arrependido se eu não tivesse ido."
+            },
+            {
+                author: "Luiz Campos",
+                date: Date.now(),
+                content: "Fernanda, eu penso exatamente a mesma coisa que sua amiga. E mesmo que as pessoas nos julguem, a opinião delas não deveriam te afetar."
+            },
+            {
+                author: "Pedro Souza",
+                date: Date.now(),
+                content: "Oi Sarah. Experimenta ir na festa, talvez você goste. Se você não gostar, mas mesmo assim tem vontade de sair com seus amigos, fala com eles e propõe um passeio que você gostaria de fazer. Muitas vezes pensamos que rolês são só festas, baladas e bares, mas há muitos outros lugares bons para ir com seus amigos."
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Perda de oportunidades",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content 3",
+        comments: [
+            {
+                author: "Cláudio Santos",
+                date: Date.now(),
+                content: "Oi pessoal! Hoje eu estava conversando com uns amigos meus e eu percebi que eu já perdi diversas oportunidades por causa da timidez. Já deixei de sair com meus amigos, já perdi oportunidades de emprego e já perdi a chance de conversar com diversas pessoas legais, tudo por causa da timidez. Isso acontece com vocês também?"
+            },
+            {
+                author: "Patrícia Silva",
+                date: Date.now(),
+                content: "Nossa, isso já aconteceu muito comigo. Na verdade, ainda acontece. Quem ai tiver um conselho, me diz como eu posso mudar isso :)"
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Me sinto descofortável em público",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content 4",
+        comments: [
+            {
+                author: "Gustavo Dias",
+                date: Date.now(),
+                content: "Sou só eu ou vocês também se sentem desconfortáveis quando estão em lugares públicos e você não está perto de pessoas que você conhece?"
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Desafios do Timidex",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content 5",
+        comments: [
+            {
+                author: "Luíza Fernandes",
+                date: Date.now(),
+                content: "Oiii gentee!!! Eu tive que sair agora aproveitei para fazer um desafio que tava aqui na minha lista. Eu tive que dar boa tarde para as pessoas na rua. Até que não foi tão ruim assim."
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Superação da timidez",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content 6",
+        comments: [
+            {
+                author: "Matheus Castro",
+                date: Date.now(),
+                content: "Ei pessoal! Abri esse fórum para contar um pouco da minha experiência com o Timidex. Eu entrei no aplicativo para tentar superar um pouco a minha timidez. Eu não vejo problema nenhum em ser tímido. O problema eram as consequeências que a timidez me trazia, porque eu não conseguia me apresentar para um público que eu não conhecia e também não me sentia confortável para conversar com outras pessoas. Quando eu comecei a usar o Timidex, eu pude ouvir conselhos de outras pessoas e conhecer pessoas com experiências parecidas com a minha. Gosto muito do aplicativo que me ajuda cada dia mais! Não superei a timidez completamente, mas estou evoluindo aos poucos."
+            }
+         ]
+    },
+    {
+        id: 7,
+        title: "Afetados pela timidez",
+        author: "Você",
+        date: Date.now(),
+        content: "Thread content 7",
+        comments: [
+            {
+                author: "João Pedro Faria",
+                date: Date.now(),
+                content: "Oi galera. Tava pensando aqui: eu já fui afetado pela timidez várias vezes, por exemplo, eu quase nunca consigo expressar minhas ideias. Como vocês já foram afetados?"
+            }
+         ]
+    }     
+]
+
+var threads = defaultThreads
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
